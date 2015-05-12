e6f63a871023cf4927e85e3975db2a903df62ead
Sliferxx <kaioken20_70@live.com>
2015-05-12 00:23:37 -0400

    Monster.h
                                     DESCRIPCIÓN
    ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
    ░Aquí definimos nuestra estructura para el tipo monstruo con sus respectivos campos ░
    ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░

---
+++ Monstruo.h
@@ -0,0 +1,16 @@
+#ifndef STDIO_H
+#include <stdio.h>
+#define STDIO_H
+#endif
+
+#ifndef STDLIB_H
+#include <stdlib.h>
+#define STDLIB_H
+#endif
+
+typedef struct T_Monstruo{
+  int vida;
+  int ataque;
+  int tesoro; // Entender que se refiere a una combinacion de puntaje ,ademas de  ptos de vida, ataque, defensa y exp que nos entregara 
+  al ser derrotado el monstruo //
+}Monstruo;
