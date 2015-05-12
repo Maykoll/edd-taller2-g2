149be2244cf0760c3448a56f90796510c53a6b83
Sliferxx <kaioken20_70@live.com>
2015-05-12 00:48:27 -0400

    Create Tesoro.h

---
+++ Tesoro.h
@@ -0,0 +1,18 @@
+#ifndef STDIO_H
+#include <stdio.h>
+#define STDIO_H
+#endif
+
+#ifndef STDLIB_H
+#include <stdlib.h>
+#define STDLIB_H
+#endif
+
+typedef struct TipoTesoro{
+  int vida;
+  int escudo;
+  int defensa;
+  int ataque;
+  int oro; 
+  int equipamiento;
+} Tesoro;
